330283e71fab3266de5561799c69ea4894caf72a
w3cj <cj@null.computer>
2019-11-17 16:51:19 -0700

    🥉 MVP

--- netflix-video-player/TODO.md
+++ netflix-video-player/TODO.md
@@ -5,8 +5,7 @@
 * [x] Hover effects on each button
   * Button background is transparent
   * On hover remove transparency
-* [ ] Basic playback controls working with JS
-* [ ] Change button icons based on player state
-* [ ] Hide title after reaching a minimum width
+* [x] Basic playback controls working with JS
+* [x] Change button icons based on player state
 * [ ] Controls dissapear after 5 seconds of no activity
 * [ ] Controls to auto appear on hover
--- netflix-video-player/app.js
+++ netflix-video-player/app.js
@@ -1,29 +1,85 @@
 const videoContainer = document.querySelector('.video-container');
 const video = document.querySelector('.video-container video');
 
-const playButton = document.querySelector('.video-container .controls button.play');
+const controlsContainer = document.querySelector('.video-container .controls-container');
+
+const playPauseButton = document.querySelector('.video-container .controls button.play-pause');
 const rewindButton = document.querySelector('.video-container .controls button.rewind');
 const fastForwardButton = document.querySelector('.video-container .controls button.fast-forward');
 const volumeButton = document.querySelector('.video-container .controls button.volume');
 const fullScreenButton = document.querySelector('.video-container .controls button.full-screen');
+const playButton = playPauseButton.querySelector('.playing');
+const pauseButton = playPauseButton.querySelector('.paused');
+const fullVolumeButton = volumeButton.querySelector('.full-volume');
+const mutedButton = volumeButton.querySelector('.muted');
+const maximizeButton = fullScreenButton.querySelector('.maximize');
+const minimizeButton = fullScreenButton.querySelector('.minimize');
 
+
+const progressBar = document.querySelector('.video-container .progress-controls .progress-bar');
 const watchedBar = document.querySelector('.video-container .progress-controls .progress-bar .watched-bar');
+const timeLeft = document.querySelector('.video-container .progress-controls .time-remaining');
 
+let controlsTimeout;
+controlsContainer.style.opacity = '0';
 watchedBar.style.width = '0px';
+pauseButton.style.display = 'none';
+minimizeButton.style.display = 'none';
+
+const displayControls = () => {
+  controlsContainer.style.opacity = '1';
+  document.body.style.cursor = 'initial';
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+  }
+  controlsTimeout = setTimeout(() => {
+    controlsContainer.style.opacity = '0';
+    document.body.style.cursor = 'none';
+  }, 5000);
+};
 
 const playPause = () => {
   if (video.paused) {
     video.play();
+    playButton.style.display = 'none';
+    pauseButton.style.display = '';
   } else {
     video.pause();
+    playButton.style.display = '';
+    pauseButton.style.display = 'none';
   }
 };
 
 const toggleMute = () => {
   video.muted = !video.muted;
+  if (video.muted) {
+    fullVolumeButton.style.display = 'none';
+    mutedButton.style.display = '';
+  } else {
+    fullVolumeButton.style.display = '';
+    mutedButton.style.display = 'none';
+  }
 };
 
-document.addEventListener('keydown', (event) => {
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+};
+
+document.addEventListener('fullscreenchange', () => {
+  if (!document.fullscreenElement) {
+    maximizeButton.style.display = '';
+    minimizeButton.style.display = 'none';
+  } else {
+    maximizeButton.style.display = 'none';
+    minimizeButton.style.display = '';
+  }
+});
+
+document.addEventListener('keyup', (event) => {
   if (event.code === 'Space') {
     playPause(); 
   }
@@ -31,13 +87,43 @@ document.addEventListener('keydown', (event) => {
   if (event.code === 'KeyM') {
     toggleMute();
   }
+
+  if (event.code === 'KeyF') {
+    toggleFullScreen();
+  }
+
+  displayControls();
+});
+
+document.addEventListener('mousemove', () => {
+  displayControls();
 });
 
 video.addEventListener('timeupdate', () => {
   watchedBar.style.width = ((video.currentTime / video.duration) * 100) + '%';
+  // TODO: calculate hours as well...
+  const totalSecondsRemaining = video.duration - video.currentTime;
+  // THANK YOU: BEGANOVICH
+  const time = new Date(null);
+  time.setSeconds(totalSecondsRemaining);
+  let hours = null;
+
+  if(totalSecondsRemaining >= 3600) {
+    hours = (time.getHours().toString()).padStart('2', '0');
+  }
+
+  let minutes = (time.getMinutes().toString()).padStart('2', '0');
+  let seconds = (time.getSeconds().toString()).padStart('2', '0');
+
+  timeLeft.textContent = `${hours ? hours : '00'}:${minutes}:${seconds}`;
 });
 
-playButton.addEventListener('click', playPause);
+progressBar.addEventListener('click', (event) => {
+  const pos = (event.pageX  - (progressBar.offsetLeft + progressBar.offsetParent.offsetLeft)) / progressBar.offsetWidth;
+  video.currentTime = pos * video.duration;
+});
+
+playPauseButton.addEventListener('click', playPause);
 
 rewindButton.addEventListener('click', () => {
   video.currentTime -= 10;
@@ -49,10 +135,4 @@ fastForwardButton.addEventListener('click', () => {
 
 volumeButton.addEventListener('click', toggleMute);
 
-fullScreenButton.addEventListener('click', () => {
-  if (!document.fullscreenElement) {
-    videoContainer.requestFullscreen();
-  } else {
-    document.exitFullscreen();
-  }
-});
+fullScreenButton.addEventListener('click', toggleFullScreen);
