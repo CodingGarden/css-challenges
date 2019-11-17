39e89a3c5a6da5a870f6a1a93ec7877c0a47e152
w3cj <cj@null.computer>
2019-11-17 15:27:40 -0700

    👷‍♀️  WIP

--- netflix-video-player/app.js
+++ netflix-video-player/app.js
@@ -7,13 +7,37 @@ const fastForwardButton = document.querySelector('.video-container .controls but
 const volumeButton = document.querySelector('.video-container .controls button.volume');
 const fullScreenButton = document.querySelector('.video-container .controls button.full-screen');
 
-playButton.addEventListener('click', () => {
+const watchedBar = document.querySelector('.video-container .progress-controls .progress-bar .watched-bar');
+
+watchedBar.style.width = '0px';
+
+const playPause = () => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
+};
+
+const toggleMute = () => {
+  video.muted = !video.muted;
+};
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Space') {
+    playPause(); 
+  }
+
+  if (event.code === 'KeyM') {
+    toggleMute();
+  }
 });
+
+video.addEventListener('timeupdate', () => {
+  watchedBar.style.width = ((video.currentTime / video.duration) * 100) + '%';
+});
+
+playButton.addEventListener('click', playPause);
 
 rewindButton.addEventListener('click', () => {
   video.currentTime -= 10;
@@ -23,9 +47,7 @@ fastForwardButton.addEventListener('click', () => {
   video.currentTime += 10;
 });
 
-volumeButton.addEventListener('click', () => {
-  video.muted = !video.muted;
-});
+volumeButton.addEventListener('click', toggleMute);
 
 fullScreenButton.addEventListener('click', () => {
   if (!document.fullscreenElement) {
